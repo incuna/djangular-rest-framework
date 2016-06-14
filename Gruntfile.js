1e2d200c76f51dc6963dde639f91af31887c62c9
Ming Lee <ming@incuna.com>
2016-06-14 11:40:02 +0100

    Add grunt file containing a lint task.

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    // Load external grunt task config.
+    grunt.loadTasks('./grunt');
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-jscs');
+    
+    grunt.initConfig({
+        config: {
+            files: {
+                lint: [
+                    'src/**/*.js'
+                ]
+            }
+        },
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            all: [
+                '<%= config.files.lint %>'
+            ]
+        },
+        jscs: {
+            options: {
+                config: '.jscsrc'
+            },
+            src: '<%= config.files.lint %>'
+        }
+    });
+
+    grunt.registerTask('lint', 'Run the JS linters.', [
+        'jshint',
+        'jscs'
+    ]);
+
+};
