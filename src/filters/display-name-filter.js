f99f09532f906cb2140b0981748f7d8980e2f32f
Ming Lee <ming@incuna.com>
2016-06-15 12:15:13 +0100

    Add filter function.

---
+++ src/filters/display-name-filter.js
@@ -0,0 +1,33 @@
+(function (angular, _) {
+    'use strict';
+
+    var drf = angular.module('drf-display-name-filter', []);
+
+    // A filter to return a display name.
+    drf.filter('drfDisplayName', function () {
+        return _.memoize(function (item, array) {
+            var displayName = item;
+
+            var foundName = _.find(array, function (arrayItem) {
+                if (item === arrayItem.value) {
+                    return arrayItem;
+                }
+            });
+
+            if (foundName) {
+                // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+                /* jshint camelcase: false  */
+                displayName = foundName.display_name;
+                /* jshint camelcase: true */
+                // jscs:enable requireCamelCaseOrUpperCaseIdentifiers
+            }
+
+            return displayName;
+
+        }, function (item, array) {
+            return item;
+        });
+
+    });
+
+}(window.angular, window._));
