d0f5eecf703067fc9baa134151d7923514a0f168
Ming Lee <ming@incuna.com>
2016-06-15 14:50:32 +0100

    Improve the comments on the filter.

--- src/filters/display-name-filter.js
+++ src/filters/display-name-filter.js
@@ -4,6 +4,25 @@
     var drf = angular.module('drf-display-name-filter', []);
 
     // A filter to return a display name.
+    // it matches the displayName from an array of objects, and returns the matching display_name.
+    // Example 
+    // choice = 'choice 1';
+    //
+    // array = [
+    //      {
+    //          value: 'choice 1',
+    //          display_name: 'Choice 1'
+    //      },
+    //      {
+    //          value: 'choice 2',
+    //          display_name: 'Choice 2'
+    //      },
+    //      {
+    //          value: 'choice 3',
+    //          display_name: 'Choice 3'
+    //      }
+    //  ];
+    // The value returned will be 'Choice 1'
     drf.filter('drfDisplayName', function () {
         return _.memoize(function (displayName, array) {
 
